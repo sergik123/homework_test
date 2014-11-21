c29dd5075e32caa9cba5077ef6ad1911f99d94e1
sergik123 <phoenix925012@yandex.ru>
2014-11-21 13:14:58 +0200

    first commit

---
+++ js/auth.js
@@ -0,0 +1,56 @@
+	// форма авторизации
+
+
+	var str       =[];
+	var flag_name = false;
+	var flag_pass = false;
+	var count     = 0;
+	str = localStorage.users ? JSON.parse(localStorage.users) : [];
+
+	$('#registration').on('click',function(){
+		window.location.href="http://homework_test/index.html#registration";
+	});
+
+	//здесь при нажатии на кнопку провер€етс€ логин и пароль с теми которые хран€тс€ в localstorage
+	//если они совпадают то мы записываем в user_id текущий id пользовател€ и заходим на его страницу
+	$('#submit_in').on('click',function(){
+
+		var name     = $('#name').val();
+		var password = $('#password').val();
+
+		for(var k = 0; k < str.length;k++){
+
+			if(str[k].user.name){
+				if (name!="") {
+					if(name == str[k].user.name){
+						flag_name = true;
+						count     = str[k].user.hash;
+						localStorage.setItem('user_id',str[k].user.id);
+
+						if (password==str[k].user.pass) {
+	     					flag_pass = true;
+	    				}
+					}
+					
+				}
+			}		
+	}
+
+	if (flag_name==true && flag_pass==true) {
+		window.location.href="http://homework_test/index.html#hash="+count;
+		location.reload();
+	};
+
+	if (password=="") {
+	     alert("¬ведите пароль пожалуйста!");		
+	    }
+
+	if(flag_name==false){
+			alert("“акой пользователь не зарегистрирован на сайте!");
+		}
+
+	if(flag_pass==false){
+			alert("ѕароль не правильный. ѕовторите попытку!");
+		}
+
+	});
